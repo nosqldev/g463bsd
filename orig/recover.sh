aab6a33894bcc07e1dc7ef778ae96984b6bc9a30
jingmi <jingmi@freebsd9.vm>
2012-06-15 16:21:37 +0800

    add recover script & source files

---
+++ orig/recover.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp ssl.h /usr/include/openssl/ssl.h
+cp ssl3.h /usr/include/openssl/ssl3.h
+cp pjdlog.c /usr/src/sbin/hastd/pjdlog.c
+cp usr_src_Makefile.inc1 /usr/src/Makefile.inc1
+cp usr_src_sys_boot_i386_Makefile.inc /usr/src/sys/boot/i386/Makefile.inc
+cp usr_src_sys_boot_i386_boot2_Makefile /usr/src/sys/boot/i386/boot2/Makefile
